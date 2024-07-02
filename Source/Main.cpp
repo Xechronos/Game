2d8339e0c756af07e379531b17291418c6aa18a2
Xechronos <abechdol@student.neumont.edu>
2024-07-02 10:16:14 -0600

    input system

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.h"
 #include "Vector2.h"
+#include "Input.h"
 
 #include <SDL.h>
 #include <iostream>
@@ -9,21 +10,45 @@ int main(int argc, char* argv[])
 {
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow("Game engine", 600, 800);
+	renderer.CreateWindow("Game engine", 800, 600);
+
+	Input input;
+	input.Initialize();
+
 
 	Vector2 v1{ 400, 300 };
 	Vector2 v2{ 600, 100 };
 
 	std::vector<Vector2> points;
-	for (int i = 0; i < 100; i++) {
+	/*for (int i = 0; i < 100; i++) {
 		points.push_back(Vector2{ rand() % 800, rand() % 600 });
-	}
-
-	while (true)
+	}*/
+	bool quit = false;
+	while (!quit)
 	{
-		Vector2 speed{ 0.1f, -0.1f };
+		/*Vector2 speed{ 0.1f, -0.1f };
 		for (Vector2& point : points) {
 			point = point + speed;
+		}*/
+		input.Update();
+
+		if (input.GetKeyDown(SDL_SCANCODE_ESCAPE)) {
+			quit = true;
+		}
+
+		Vector2 mousePosition = input.GetMousePosition();
+		std::cout << mousePosition.x <<  " " << mousePosition.y << std::endl;
+
+		if (input.GetMouseButtonDown(0) && !input.GetPrevMouseButtonDown(0)) {
+			std::cout << "mouse pressed" << std::endl;
+			points.push_back(mousePosition);
+		}
+
+		if (input.GetMouseButtonDown(0) && input.GetPrevMouseButtonDown(0)) {
+			float distance = (points.back() - mousePosition).Length();
+			if (distance > 5) {
+				points.push_back(mousePosition);
+			}
 		}
 
 		//	// clear screen
@@ -38,15 +63,16 @@ int main(int argc, char* argv[])
 		renderer.DrawLine(500, 400, 300, 400);
 		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
 
-		for (int i = 0; i < points.size() - 1; i++) {
+		for (int i = 0; points.size() >  1 && i < points.size() - 1; i++) {
+			renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 0);
 			renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
 		}
 
-		for (int i = 0; i < 200; i++) {
+		/*for (int i = 0; i < 200; i++) {
 			renderer.DrawLine(rand() % 300, rand() % 200, rand() % 800, rand() % 600);
 			renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 0);
 			renderer.DrawPoint(rand() % 400, rand() % 300);
-		}
+		}*/
 
 		renderer.EndFrame();
 	}
